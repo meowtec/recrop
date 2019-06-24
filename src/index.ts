2f652fef9a8799ff5cf25b72f4b44bc1453e1df5
meowtec <bertonzh@gmail.com>
2019-06-24 15:14:40 +0800

    export alias

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,17 @@ import ImageCrop, { ImageCropProps, ResizeObserve } from './ImageCrop'
 import MultipleSelection, { MultipleSelectionProps } from './MultipleSelection'
 import { Rect } from './utils'
 
+type Crop = Rect
+type RegionSelection = Selection
+type RegionSelectionProps = SelectionProps
+
 export {
+  Crop,
   Rect,
   Selection,
+  RegionSelection,
   SelectionProps,
+  RegionSelectionProps,
   ImageCrop,
   ImageCropProps,
   ResizeObserve,
